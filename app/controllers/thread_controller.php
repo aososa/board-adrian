f84b5bf155c8c0cf225c534cafb305b18786bd06
aososa <a.sosa@klab.com>
2014-10-17 10:27:30 +0800

    Added functionality for posting comments.

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -12,5 +12,26 @@ class ThreadController extends AppController {
 		$this->set(get_defined_vars());
 	}
 
+	public function write() {
+		$thread = Thread::get(Param::get('thread_id'));
+		$comment = new Comment;
+		$page = Param::get('page_next');
+
+		switch ($page) {
+			case 'write_end':
+			   $comment->username = Param::get('username');
+			   $comment->body = Param::get('body');
+			   $thread->write($comment);
+			   break;
+			
+			default:
+			   throw new NotFoundException("{$page} is not found");
+			   break;
+		}
+		
+		$this->set(get_defined_vars());
+		$this->render($page);
+	}
+
 }
 ?>
