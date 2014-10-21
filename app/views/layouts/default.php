fb4c46fad79a4991dfd7dcadc344d69dc3fea4af
aososa <a.sosa@klab.com>
2014-10-21 12:05:23 +0800

    Added header links

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -17,7 +17,9 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="brand" href="#">DietCake Hello</a>
+          <a class="brand" href="<?php outputText(url('thread/index'))?>">DietCake Hello</a>
+          <a class="brand" href="<?php outputText(url('thread/create'))?>">Create Thread</a>
+          <a class="brand" href="<?php outputText(url('user/add'))?>">Add User</a>
         </div>
       </div>
     </div>
