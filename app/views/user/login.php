4e4e3b668136f35988ec339ac7ec1e8f6e8d4958
aososa <a.sosa@klab.com>
2014-10-23 10:03:12 +0800

    Improved user registration flow.

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -1,10 +1,15 @@
 <h1>Login</h1>
+<?php if(!empty($login_from_add)): ?>
+    <p class="alert alert-success">
+        Account registered. Try logging in. :)
+    </p>
+<?php endif ?>
 <h3>Please type your username and password</h3>
 <form class="well" method="post" action="<?php outputText(url('user/login'))?>">
    <label>Username</label>
    <input type="text" class="span2" name="username" value="<?php outputText(Param::get('username')) ?>">
    <label>Password</label>
-   <input type="password" class="span2" name="password" value="<?php outputText(Param::get('password'))?>">
+   <input type="password" class="span2" name="password">
    <br />
    <input type="hidden" name="page_next" value="home">
    <button type="submit" class="Btn btn-primary">Submit</button>
