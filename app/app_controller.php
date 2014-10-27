17fe9a0ab69f0ca3eabf50fd0251542180343d74
aososa <a.sosa@klab.com>
2014-10-27 16:53:08 +0800

    Used single quotes in allowed actions array

--- app/app_controller.php
+++ app/app_controller.php
@@ -4,7 +4,7 @@ class AppController extends Controller
     public $default_view_class = 'AppLayoutView';
 
     public function beforeFilter() {
-        $allowed_actions = array("user");
+        $allowed_actions = array('user');
         if(!in_array($this->name, $allowed_actions) && !is_logged_in()) {
             redirect($controller = 'user');
         }
